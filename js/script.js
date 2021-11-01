4feac7e1279b37f67684aeb699bb9c2f57dc6fcd
edgar9112 <edgarsanchez9112@gmail.com>
2021-11-01 15:44:23 -0700

    tienda hercules

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let loginForm = document.querySelector('.header .login-form');
+let nabvar = document.querySelector('.header .navbar');
+document.querySelector('#login-btn').onclick= () => {
+    loginForm.classList.toggle('active');
+    nabvar.classList.remove('active');
+}
+document.querySelector('#menu-btn').onclick= () => {    
+    nabvar.classList.toggle('active');
+    loginForm.classList.remove('active');
+}
+window.onscroll = () => {
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+        
+    
+}
+window.onload = () => {
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    }else {
+        document.querySelector('.header').classList.remove('active');
+    }
+}
